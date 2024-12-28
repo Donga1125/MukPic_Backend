e5f0f454aca586119d7dc9b84b1dcaf2a7cd49eb
pyan <pa98213@gmail.com>
2024-12-28 20:55:19 +0900

    login - jwt v

--- mukpic/src/main/java/i4U/mukPic/user/dto/LoginRequestDTO.java
+++ mukpic/src/main/java/i4U/mukPic/user/dto/LoginRequestDTO.java
@@ -3,10 +3,8 @@ package i4U.mukPic.user.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor
 public class LoginRequestDTO {
 
--- mukpic/src/main/java/i4U/mukPic/user/service/LoginService.java
+++ mukpic/src/main/java/i4U/mukPic/user/service/LoginService.java
@@ -1,37 +1,21 @@
 package i4U.mukPic.user.service;
 
-import i4U.mukPic.global.exception.BusinessLogicException;
-import i4U.mukPic.global.exception.ExceptionCode;
+import i4U.mukPic.global.jwt.security.JwtTokenProvider;
+import i4U.mukPic.global.jwt.service.AuthenticationService;
 import i4U.mukPic.user.dto.LoginRequestDTO;
 import i4U.mukPic.user.entity.User;
-import i4U.mukPic.user.entity.UserStatus;
-import i4U.mukPic.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LoginService {
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationService authenticationService;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public User login(LoginRequestDTO loginRequest) {
-        // userId로 사용자 조회
-        User user = userRepository.findByUserId(loginRequest.getUserId())
-                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.USER_NOT_FOUND));
-
-        // 비밀번호 검증
-        if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
-            throw new BusinessLogicException(ExceptionCode.INVALID_CREDENTIALS);
-        }
-
-        // 사용자 상태 확인
-        if (user.getUserStatus() != UserStatus.ACTIVE) {
-            throw new BusinessLogicException(ExceptionCode.USER_NOT_ACTIVE);
-        }
-
-        return user;
+    public String login(LoginRequestDTO loginRequest) {
+        User user = authenticationService.authenticate(loginRequest.getUserId(), loginRequest.getPassword());
+        return jwtTokenProvider.generateToken(user.getUserId());
     }
 }
