83e8541dfeb2f86205b4b656fc6d9acaad9a1587
Donga <96361421+Donga1125@users.noreply.github.com>
2024-12-28 22:36:20 +0900

    github action test

--- mukpic/src/main/java/i4U/mukPic/community/controller/CommunityController.java
+++ mukpic/src/main/java/i4U/mukPic/community/controller/CommunityController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/community")
 public class CommunityController {
+    //test
     @GetMapping
     public ResponseEntity<String> getAllFeed() {
             return ResponseEntity.ok("test");
