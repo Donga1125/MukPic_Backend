464e39aeab138ad1a8900920ea7493a26d6b3e46
Donga <96361421+Donga1125@users.noreply.github.com>
2025-01-08 00:15:13 +0900

    Update WebMvcConfig.java

--- mukpic/src/main/java/i4U/mukPic/global/config/WebMvcConfig.java
+++ mukpic/src/main/java/i4U/mukPic/global/config/WebMvcConfig.java
@@ -16,7 +16,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8000") // AI 서버 주소
+                .allowedOrigins("http://localhost:3000","http://localhost:8000") // AI 서버 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
