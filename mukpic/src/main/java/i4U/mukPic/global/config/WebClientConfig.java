b5a932d471a81395307f7469b9b92d6bfe5ee1f5
pyan <pa98213@gmail.com>
2025-01-11 17:14:13 +0900

    fix: 환경변수 처리 #16

--- mukpic/src/main/java/i4U/mukPic/global/config/WebClientConfig.java
+++ mukpic/src/main/java/i4U/mukPic/global/config/WebClientConfig.java
@@ -1,11 +1,15 @@
 package i4U.mukPic.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+
+    @Value("${ai.base-url}")
+    private String aiBaseUrl;
 
     @Bean
     public WebClient.Builder webClientBuilder() {
@@ -14,6 +18,6 @@ public class WebClientConfig {
 
     @Bean
     public WebClient webClient(WebClient.Builder builder) {
-        return builder.baseUrl("https://mukpicai.duckdns.org").build();
+        return builder.baseUrl(aiBaseUrl).build();
     }
 }
